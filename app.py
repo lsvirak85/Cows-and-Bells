63465e4e694c8f1b8a2fe42d2267268e072c84f7
lsvirak85 <lsvirak85@gmail.com>
2023-03-11 22:15:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
 import random
 
-# generate a random 4-digit number
-def generate_number():
-    digits = list(range(10))
-    random.shuffle(digits)
-    return digits[:4]
-
-def count_cows_and_bulls(secret_number, guess):
-    cows = 0
-    bulls = 0
-    for i in range(4):
-        if secret_number[i] == guess[i]:
-            cows += 1
-        elif guess[i] in secret_number:
-            bulls += 1
-    return cows, bulls
 
 def play_game():
-    secret_number = generate_number()
-    num_guesses = 0
-    while True:
-        num_guesses += 1
-        guess = input("Enter a 4-digit number: ")
-        guess = [int(d) for d in guess]
-        cows, bulls = count_cows_and_bulls(secret_number, guess)
-        print("Cows:", cows, "Bulls:", bulls)
-        if cows == 4:
-            print("Congratulations! You guessed the number in", num_guesses, "guesses.")
-            break
+    def cows_and_bulls():
+        # randomly generate a 4-digit number
+        number = str(random.randint(1000, 9999))
+        print("Let's play the Cows and Bulls game!")
+        print("Guess a 4-digit number.")
 
-if __name__ == '__main__':
-    play_game()
+        # set the maximum number of guesses
+        max_guesses = 5
+        num_guesses = 0
+
+        # start the game loop
+        while num_guesses < max_guesses:
+            # ask the user for a guess
+            guess = input("Enter your guess: ")
+            num_guesses += 1
+
+            # check if the guess is correct
+            if guess == number:
+                print("Congratulations! You guessed the number in", num_guesses, "guesses.")
+                return
+
+            # check for cows and bulls
+            cows = 0
+            bulls = 0
+            for i in range(4):
+                if guess[i] == number[i]:
+                    cows += 1
+                elif guess[i] in number:
+                    bulls += 1
+
+            # print the number of cows and bulls
+            print(cows, "cows,", bulls, "bulls")
+
+        # if the user has run out of guesses, display the answer
+        print("Sorry, you have run out of guesses. The answer was", number)
+
+    cows_and_bulls()
+
+
+play_game()
